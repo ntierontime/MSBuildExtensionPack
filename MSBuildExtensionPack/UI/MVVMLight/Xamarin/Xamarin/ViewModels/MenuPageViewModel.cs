a8f44acd824cb645d8287cf624c3a36cc795b8ac
David Zhao <ntierontime@gmail.com>
2017-04-25 15:42:21 -0400

    adding pages.

--- MSBuildExtensionPack/UI/MVVMLight/Xamarin/Xamarin/ViewModels/MenuPageViewModel.cs
+++ MSBuildExtensionPack/UI/MVVMLight/Xamarin/Xamarin/ViewModels/MenuPageViewModel.cs
@@ -7,21 +7,32 @@ namespace Xamarin {
 
 		public ICommand GoHomeCommand { get; set; }
 		public ICommand GoSecondCommand { get; set; }
-
-		public MenuPageViewModel() {
+        public ICommand GoBuildLogCreateCommand { get; set; }
+        public MenuPageViewModel() {
 			GoHomeCommand = new Command(GoHome);
 			GoSecondCommand = new Command(GoSecond);
-		}
+            GoBuildLogCreateCommand = new Command(GoBuildLogCreate);
 
-		void GoHome(object obj) {
-			App.NavigationPage.Navigation.PopToRootAsync();
-			App.MenuIsPresented = false;
-		}
+        }
 
-		void GoSecond(object obj) {
+        void GoHome(object obj)
+        {
+            App.NavigationPage.Navigation.PopToRootAsync();
+            App.MenuIsPresented = false;
+        }
+
+        void GoSecond(object obj) {
 			App.NavigationPage.Navigation.PushAsync(new SecondPage());
 			App.MenuIsPresented = false;
 		}
-	}
+
+
+        void GoBuildLogCreate(object obj)
+        {
+            App.NavigationPage.Navigation.PushAsync(new Xamarin.Pages.BuildLog.Create());
+            App.MenuIsPresented = false;
+        }
+
+    }
 }
 
