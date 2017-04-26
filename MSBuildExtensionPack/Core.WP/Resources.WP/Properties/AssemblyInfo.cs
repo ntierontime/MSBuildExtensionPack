dd5b9e019490873c476aa1dfbf942c5e62e32dbb
David Zhao <ntierontime@gmail.com>
2017-04-26 15:07:30 -0400

    added winrt and windows phone 8.1 app

--- MSBuildExtensionPack/Core.WP/Resources.WP/Properties/AssemblyInfo.cs
+++ MSBuildExtensionPack/Core.WP/Resources.WP/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -35,3 +36,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: InternalsVisibleTo("Xamarin")]
