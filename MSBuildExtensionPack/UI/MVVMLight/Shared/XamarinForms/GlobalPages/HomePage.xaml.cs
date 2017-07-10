1128c5b9818f2170c5f1e647a020d68b83cc6b80
Administrator <ntierontime@gmail.com>
2017-07-09 21:38:30 -0400

    something new.

---
+++ MSBuildExtensionPack/UI/MVVMLight/Shared/XamarinForms/GlobalPages/HomePage.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MSBuildExtensionPack.XamarinForms.GlobalPages {
+	public partial class HomePage : ContentPage {
+		public HomePage() {
+			InitializeComponent();
+		}
+	}
+}
+
