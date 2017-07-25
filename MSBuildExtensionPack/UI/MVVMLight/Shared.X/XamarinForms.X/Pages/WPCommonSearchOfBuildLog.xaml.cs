0db2de1430982b76b57a02932ee6ed7786361417
david4chao <david4chao@gmail.com>
2017-07-25 15:54:34 -0400

    adding search page.

---
+++ MSBuildExtensionPack/UI/MVVMLight/Shared.X/XamarinForms.X/Pages/WPCommonSearchOfBuildLog.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MSBuildExtensionPack.XamarinForms.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WPCommonSearchOfBuildLog : ContentPage
+	{
+		public WPCommonSearchOfBuildLog()
+		{
+			InitializeComponent();
+		}
+	}
+}
