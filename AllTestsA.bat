a6225cbc7af89633f079573664a169654524df62
Administrator <ntierontime@gmail.com>
2017-11-19 20:38:16 -0500

    test2

--- AllTestsA.bat
+++ AllTestsA.bat
@@ -10,13 +10,10 @@ RD Frameworks.X /S /Q
 RD %database% /S /Q
 DEL *.sln /S /Q
 
-
 D:\Test4\WhatEverII\trunk\BuiltResults\Dbml.CommandLine\nTierCraft.T4Parser.FileBuilders.Generated.Dbml.CommandLine.exe -o LoadQuickCodeGenConfigurationCmdLineArgPerSolution -r "D:\Github\%database%"
 
 git add -A
-
 git commit -m test2
-
 git push
 
 copy "D:\Test4\WhatEverII\trunk\TestCertificates\UWPApp_TemporaryKey.pfx.copy" %database%\UI\MVVMLight\UWPApp\%database%.UWPApp_TemporaryKey.pfx
@@ -32,3 +29,7 @@ copy "D:\Test4\WhatEverII\trunk\TestCertificates\UWPApp_TemporaryKey.pfx.copy" %
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %database%\UI\MVVMLight\WPF4App\%database%.WPF4App.csproj /p:Configuration=Release
 
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %database%\UI\Web\AspNetMvc40Rasor\%database%.AspNetMvc40Rasor.csproj /p:Configuration=Release
+
+git add -A
+git diff --quiet && git diff --staged --quiet || git commit -m 'test'
+git push
