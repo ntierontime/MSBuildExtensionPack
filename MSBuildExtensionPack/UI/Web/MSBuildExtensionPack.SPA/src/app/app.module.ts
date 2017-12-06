c1c226a232cd444e07d380a300b04252f3f4c62f
David Zhao <ntierontime@gmail.com>
2017-12-06 14:15:32 -0500

    added buildlog

--- MSBuildExtensionPack/UI/Web/MSBuildExtensionPack.SPA/src/app/app.module.ts
+++ MSBuildExtensionPack/UI/Web/MSBuildExtensionPack.SPA/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { BuildlogComponent } from './buildlog/buildlog.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, BuildlogComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ MSBuildExtensionPack/UI/Web/MSBuildExtensionPack.SPA/src/app/buildlog/buildlog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cm-buildlog',
+  templateUrl: './buildlog.component.html',
+  styleUrls: ['./buildlog.component.css']
+})
+export class BuildlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+    hero = 'Windstorm';
+}
